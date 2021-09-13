b82a73567ff574fc3cc06faa366921ce8b0d9da3
Ricardo Lelis <ricardo.lelis3@gmail.com>
2021-09-13 23:43:00 +0100

    Added mongoDB query through RestaurantsController.

--- backend/api/restaurants.route.js
+++ backend/api/restaurants.route.js
@@ -3,6 +3,6 @@ import RestaurantsCtrl from "./restaurants.controller.js"
 
 const router = express.Router()
 
-router.route("/").get(RestaurantsCtrl.api.GetRestaurants)
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
 
 export default router
